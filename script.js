9e74c5c332217eaeedacd65fdeb1f5b35f438d9d
aleksandra-zagorovskaya <aleksandrazagorovskaya@gmail.com>
2019-06-11 20:04:13 +0300

    version 1

---
+++ script.js
@@ -0,0 +1,32 @@
+//задача1
+var i;
+for (i = 1; i <= 50; i++) {
+    console.log( i );
+}
+for (i = 35; i >= 8; i--) {
+    console.log( i );
+}
+
+//задача2
+var x=89;
+while (x>=11) {
+    console.log(x);
+    x--;
+}
+
+//задача3
+var i=0;
+var sum=0;
+for (i;i<=100; i++){
+    sum+=i;
+}
+console.log (sum);
+
+//задача4
+var sum=0;
+for (i=1;i<=5; i++)
+    sum+=i;
+console.log (sum);
+
+
+//задача5
